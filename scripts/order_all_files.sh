9557948a780594731d476b98e6d5cfb82a321e92
Jeffrey Van Mulem <jeffreyvanmulem@gmail.com>
2018-02-16 10:22:40 +0100

    order all files

--- scripts/order_all_files.sh
+++ scripts/order_all_files.sh
@@ -6,7 +6,7 @@ cd /media/webcam/motion
 #list all dates and hours
 for folder in $( ls -p | sed "s/[0-9][0-9][0-9][0-9]-.*//" | sort | uniq | grep -v / | tr _ / );
 do
-    foldername = $folder\_00
+    foldername=$folder\_00
 
     if [ ! -d $foldername ]; then
         mkdir $foldername -p
