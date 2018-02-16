9e34886b86b19a0077b52fbc9fe6547f7fa257df
Jeffrey Van Mulem <jeffreyvanmulem@gmail.com>
2018-02-16 10:20:29 +0100

    order all files

---
+++ scripts/order_all_files.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+cd /media/webcam/motion
+
+#list all dates and hours
+for folder in $( ls -p | sed "s/[0-9][0-9][0-9][0-9]-.*//" | sort | uniq | grep -v / | tr _ / );
+do
+    foldername = $folder\_00
+
+    if [ ! -d $foldername ]; then
+        mkdir $foldername -p
+        echo created folder $foldername
+    fi
+
+done;
